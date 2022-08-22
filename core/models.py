635777fdf65f07a10019091f9d9b2ae11ceabc51
joaoluca1214 <joaoluca1214@gmail.com>
2022-08-22 15:11:50 -0300

    22/08/22

---
+++ core/models.py
@@ -0,0 +1,44 @@
+from tabnanny import verbose
+from django.db import models
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome 
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+class Categoria(models.Model):  
+    descricao = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.descricao
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+
+class Livro(models.Model):
+
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+ 
+    def __str__(self):
+        return f'{self.titulo} ({self.quantidade}) - R${self.preco}'
